2db977a2e5fb590d5d5062891cbd5c0cef09f967
ValerioScharizza <valerio.schiarizza@live.it>
2022-10-30 16:07:28 +0100

    starting footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.dc-layout')
 
 @section('main-content')
-    <h1>Titolone di prova</h1>
+    <h1></h1>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,18 @@
-FOOTER
+<footer>
+    <div id="footer-top" style="background-image: url({{ asset('images/footer-bg.jpg') }});
+    background-size: cover;">
+        <div class="container">
+            <div>
+                FOOTER LINKS
+            </div>
+            <div style="background-image: url({{ asset('images/dc-logo-bg.png') }})">
+
+            </div>
+        </div>
+    </div>
+
+
+    <div id="footer-bottom";>
+
+    </div>
+</footer>
