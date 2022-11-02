2eb2b12c5b6b18a220f8511e3517cd3aff6d2dda
ValerioScharizza <valerio.schiarizza@live.it>
2022-11-02 10:33:49 +0100

    finished main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.dc-layout')
 
 @section('main-content')
-    <h1>PAGE CONTENT</h1>
+    <div class="dark-bg">
+        <div class="container comics-container-main">
+            <div>
+                <h3>CURRENT SERIES</h3>
+            </div>
+
+            @foreach ($comics as $comic)
+                <div class="comics-card">
+
+                    <img class="comic-card-img" src="{{ $comic['thumb'] }}" alt="">
+
+                    <p>{{ $comic['series'] }}</p>
+                </div>
+            @endforeach
+
+            <button>LOAD MORE</button>
+        </div>
+    </div>
 @endsection
